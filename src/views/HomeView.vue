b677e027cb115e5ad11645f2887f6b880e63ded8
jpgomesf <jppromus@gmail.com>
2023-03-13 16:00:35 -0300

    (emiolo-fe): Basic HomeView - Display cards

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,111 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <section class="bg-white dark:bg-gray-900">
+    <div class="flex lg:min-h-screen items-center justify-center">
+
+      <div class="flex container space-x-4">
+
+        <div class="flex-col">
+          <article class="overflow-hidden rounded-lg border border-gray-400 shadow-sm">
+            <img alt="Office"
+              src="https://images4.alphacoders.com/675/thumb-1920-675554.jpg"
+              class="h-70 w-full object-cover" />
+  
+            <div class="p-4 sm:p-6">
+              <a>
+                <h3 class="text-xl font-bold text-gray-900 dark:text-white sm:text-2xl md:text-xl ">
+                  How much do you know about the characters?
+                </h3>
+              </a>
+  
+              <p class="mt-2 text-sm leading-relaxed text-gray-500 line-clamp-3 dark:text-gray-400">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
+                dolores, possimus pariatur animi temporibus nesciunt praesentium dolore
+                sed nulla ipsum eveniet corporis quidem, mollitia itaque minus soluta,
+                voluptates neque explicabo tempora nisi culpa eius atque dignissimos.
+                Molestias explicabo corporis voluptatem?
+              </p>
+  
+              <a class="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-blue-600">
+                Find out more
+  
+                <span aria-hidden="true" class="block transition group-hover:translate-x-0.5">
+                  &rarr;
+                </span>
+              </a>
+            </div>
+          </article>
+  
+        </div>
+  
+        <div class="flex-col">
+          <article class="overflow-hidden rounded-lg border border-gray-400  shadow-sm">
+            <img alt="Office"
+              src="https://wallup.net/wp-content/uploads/2018/03/20/395923-Star_Wars-spaceship-render-CGI-artwork-simple_background.jpg"
+              class="h-70 w-full object-cover" />
+  
+              <div class="p-4 sm:p-6">
+              <a>
+                <h3 class="text-xl font-bold text-gray-900 dark:text-white sm:text-2xl md:text-xl ">
+                  How much do you know about the characters?
+                </h3>
+              </a>
+  
+              <p class="mt-2 text-sm leading-relaxed text-gray-500 line-clamp-3 dark:text-gray-400">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
+                dolores, possimus pariatur animi temporibus nesciunt praesentium dolore
+                sed nulla ipsum eveniet corporis quidem, mollitia itaque minus soluta,
+                voluptates neque explicabo tempora nisi culpa eius atque dignissimos.
+                Molestias explicabo corporis voluptatem?
+              </p>
+  
+              <a class="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-blue-600">
+                Find out more
+  
+                <span aria-hidden="true" class="block transition group-hover:translate-x-0.5">
+                  &rarr;
+                </span>
+              </a>
+            </div>
+          </article>
+        </div>
+  
+        <div class="flex-col">
+          <article class="overflow-hidden rounded-lg border border-gray-400 shadow-sm">
+            <img alt="Office"
+              src="https://i.imgur.com/5Ke5t6f.jpg"
+              class="h-70 w-full object-cover" />
+  
+              <div class="p-4 sm:p-6">
+              <a>
+                <h3 class="text-xl font-bold text-gray-900 dark:text-white sm:text-2xl md:text-xl ">
+                  How much do you know about the characters?
+                </h3>
+              </a>
+  
+              <p class="mt-2 text-sm leading-relaxed text-gray-500 line-clamp-3 dark:text-gray-400">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
+                dolores, possimus pariatur animi temporibus nesciunt praesentium dolore
+                sed nulla ipsum eveniet corporis quidem, mollitia itaque minus soluta,
+                voluptates neque explicabo tempora nisi culpa eius atque dignissimos.
+                Molestias explicabo corporis voluptatem?
+              </p>
+  
+              <a class="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-blue-600">
+                Find out more
+  
+                <span aria-hidden="true" class="block transition group-hover:translate-x-0.5">
+                  &rarr;
+                </span>
+              </a>
+            </div>
+          </article>
+        </div>
+      </div>
+
+
+    </div>
+  </section>
 </template>
