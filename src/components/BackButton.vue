d1ea52a629feeb543593a7de530d7343d0415657
jpgomesf <jppromus@gmail.com>
2023-03-14 18:55:29 -0300

    (emiolo-fe) BackButton - redirects user to home pg

---
+++ src/components/BackButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container mx-auto flex items-center">
+        <RouterLink to="/" class="group relative inline-block overflow-hidden border border-indigo-600 px-7 py-2 focus:outline-none focus:ring"
+        >
+            <span
+                class="absolute inset-x-0 bottom-0 h-[2px] bg-indigo-600 transition-all group-hover:h-full group-active:bg-indigo-500"></span>
+
+            <span class="relative text-sm font-medium text-indigo-600 transition-colors group-hover:text-white">
+                Go back!
+            </span>
+        </RouterLink>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
