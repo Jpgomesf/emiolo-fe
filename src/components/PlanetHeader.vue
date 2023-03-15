16a322dafef343fea24a6d157e64eabdd10a07b0
jpgomesf <jppromus@gmail.com>
2023-03-15 00:10:03 -0300

    (emiolo-fe) Componentization of PlanetsView new PlanetHeader and PlanetDescription components

---
+++ src/components/PlanetHeader.vue
@@ -0,0 +1,48 @@
+<template>
+        <div class="relative block overflow-hidden rounded-t-md dark:bg-gray-800 p-4 sm:p-6 lg:p-8">
+            <div class="container mt-1 mx-auto">
+                <h1 class="text-2xl font-bold text-gray-900 dark:text-white sm:text-3xl">
+                    Exploring the Wondrous Planets of the Star Wars Universe
+                </h1>
+
+                <div class="mt-4 text-md leading-relaxed text-gray-300 dark:text-gray-200 space-y-2">
+                    <p>
+                        The Star Wars universe is home to a vast array of planets, each with its own distinctive geography,
+                        climate, and inhabitants. Planets play a crucial role in the story, serving as the settings for
+                        battles, negotiations, and other important events.
+                        Characters frequently travel between planets using the various spaceships at their disposal,
+                        allowing them to explore new worlds and encounter new challenges.
+                        From the desert world of Tatooine to the ice planet of Hoth, the planets of Star Wars are diverse
+                        and fascinating, and offer a rich tapestry for the story to unfold upon.
+                    </p>
+                    <p>
+                        Moreover, they are integral to the story, serving as the settings for battles, negotiations, and
+                        other important events. The desert planet of Tatooine, for example, is where Luke Skywalker grew up
+                        and first encountered the droids that set him on his journey.
+                        The ice planet of Hoth is the site of a major battle between the Rebel Alliance and the Galactic
+                        Empire.
+                    </p>
+                    <p>
+                        Characters frequently travel between planets using the various spaceships at their disposal,
+                        allowing them to explore new worlds and encounter new challenges.
+                        This adds to the sense of adventure and excitement that pervades the Star Wars universe, as the
+                        characters are constantly on the move, exploring new worlds and encountering new threats.
+                    </p>
+                    <p>
+                        Overall, the diverse and fascinating planets of Star Wars are a crucial component of the franchise's
+                        enduring appeal.
+                        They offer a rich tapestry for the story to unfold upon, and they allow audiences to explore new and
+                        exciting worlds alongside their favorite characters.
+                    </p>
+                </div>
+            </div>
+        </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
