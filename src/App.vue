6e5e42b9a51ea578d1a83307484249ec048999b0
jpgomesf <jppromus@gmail.com>
2023-03-14 23:07:34 -0300

    (emiolo-fe) Updated README.md + comment removed

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,6 @@ const visibleStore = useVisibleStore();
 
 const isVisible = computed(() => visibleStore.isVisible);
 
-
-// const callback: CallbackTypes.CredentialCallback  = (res) => {
-//   const userData: any = decodeCredential(res.credential)
-//   console.log("res", userData)
-//   console.log("name", userData.name, userData.email, userData.picture)
- 
-// }
 </script>
 
 <template>
